2eae3e57b514b4081b26180e96a00dce49c08639
Qazi Mustafa <122636931+Qazi-Mustafa@users.noreply.github.com>
2024-11-20 23:38:04 +0500

    Add files via upload

--- SchedUTrack/backend/index.js
+++ SchedUTrack/backend/index.js
@@ -6,6 +6,7 @@ const authRoute = require('./routes/auth');
 const courseRoute = require('./routes/course');
 const taskRoute = require('./routes/task'); 
 const addTaskRoute = require('./routes/addTask'); // Import the addTask route
+const attendanceRoutes = require('./routes/attendance');
 
 const app = express();
 
@@ -24,25 +25,10 @@ app.use('/api/auth', authRoute);
 app.use('/api/courses', courseRoute);
 app.use('/api/tasks', taskRoute);
 app.use('/api/add-task', addTaskRoute); // Add the add-task route
+app.use('/api/attendance', attendanceRoutes);
 
 app.get('/', (req, res) => {
     res.send('Server is running');
-});
-
-app.get('/api/auth', (req, res) => {
-    res.send('Login is running');
-});
-
-app.get('/api/courses', (req, res) => {
-    res.send('Courses is running');
-});
-
-app.get('/api/tasks', (req, res) => {
-    res.send('tasks is running');
-});
-
-app.get('/api/add-task', (req, res) => {
-    res.send('add-task is running');
 });
 
 const PORT = process.env.PORT || 5000;
